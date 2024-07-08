e5aa9115ec00a08473a2ef8738fad3ad935db84c
bobray <bobray@softville.com>
2024-07-07 23:18:42 -0500

    Re-do build.config.php

---
+++ _build/build.config.php
@@ -0,0 +1,18 @@
+<?php
+
+ /* Define the MODX path constants for build.transport.php */
+if (!defined('MODX_CORE_PATH')) {
+    define('MODX_CORE_PATH', dirname(__FILE__,4) . '/core/');
+    define('MODX_BASE_PATH', dirname(__FILE__, 4) . '/');
+    define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
+    define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
+    define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
+}
+
+/* not used -- here to prevent E_NOTICE warnings */
+if (!defined('MODX_BASE_URL')) {
+    define('MODX_BASE_URL', 'http://localhost/addons/');
+    define('MODX_MANAGER_URL', 'http://localhost/addons/manager/');
+    define('MODX_ASSETS_URL', 'http://localhost/addons/assets/');
+    define('MODX_CONNECTORS_URL', 'http://localhost/addons/connectors/');
+}
